15dc473bb36cc7386ff8b0cfdcbfc5a97402e50c
premgit-hub <premanand.kumar@outlook.com>
2021-07-27 17:45:04 +0530

    Init

---
+++ BlazorEmployeeManagement/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Department { get; set; }       
+
+    }
+}
